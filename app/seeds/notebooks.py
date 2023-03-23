40021fa083c4cec015cd252f4867fe2cce78a47b
Eunice Park <eunicexpark01@gmail.com>
2023-03-22 23:31:23 -0700

    seed data works whoo

---
+++ app/seeds/notebooks.py
@@ -0,0 +1,3 @@
+nb1 = Notebook(name="Hello Kitty and Friends", created_at='02/12/2023', owner_id=1)
+    # nb2 = Notebook(name="Kuromi and My Melody fighting", created_at='01/05/2023', owner_id=2)
+    # nb3 = Notebook(name="My Melody and Friends", created_at='12/25/2022', owner_id=3)
