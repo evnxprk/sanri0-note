a8116c723913640526993f405b1ef964e301bfd0
Eunice Park <eunicexpark01@gmail.com>
2024-06-21 19:58:26 -0400

    updated the functionality for features

--- app/models/notebook.py
+++ app/models/notebook.py
@@ -1,26 +1,3 @@
-# from .db import db, environment, SCHEMA, add_prefix_for_prod
-# from datetime import datetime
-
-# class Notebook(db.Model):
-#     __tablename__ = 'notebooks'
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
-#     id = db.Column(db.Integer, primary_key=True)
-#     owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
-#     name = db.Column(db.String)
-#     created_at = db.Column(db.DateTime, default=datetime.utcnow())
-    
-#     owner = db.relationship('User', back_populates='notebooks')
-#     notes = db.relationship('Note', back_populates='notebook')
-    
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'owner_id': self.owner_id,
-#             'name': self.name,
-#             'created_at': self.created_at
-#         }
-
 
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
